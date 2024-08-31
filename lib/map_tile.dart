a2e93267da6ef32806b2dd2cb3d92475c41a571a
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-09-01 01:38:50 +0200

    Show map deletion confirmation dialog

--- lib/map_tile.dart
+++ lib/map_tile.dart
@@ -5,6 +5,7 @@ import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:path/path.dart" as p;
 
 import "dual_pane.dart";
+import "utils.dart";
 
 class MapTile extends ConsumerWidget {
   final File configFile;
@@ -19,15 +20,60 @@ class MapTile extends ConsumerWidget {
       trailing: IconButton(
         icon: const Icon(Icons.delete),
         onPressed: () {
-          //TODO: Show confirmation dialog
+          showDialog<bool>(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text("Delete map"),
+              content: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Wrap(
+                    children: [
+                      const Text("Are you sure you want to delete the map \" "),
+                      Text(
+                        _toHuman(configFile),
+                        style: pixelCode.copyWith(height: 1.4),
+                      ),
+                      const Text(" \" ?"),
+                    ],
+                  ),
+                  const Text(
+                    "This action cannot be undone!",
+                    style: TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                  const Text(
+                    "However, you can just add the map again, as no unrecoverable data will be deleted.",
+                  ),
+                  const Text(
+                    "Your Minecraft world data will not be affected by this action, only the BlueMap data.",
+                  )
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  child: const Text("Delete"),
+                ),
+                ElevatedButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: const Text("Cancel"),
+                ),
+              ],
+            ),
+          ).then((bool? confirmed) {
+            if (confirmed == null || confirmed == false) return;
 
-          //close the editor if it's open on that file
-          if (openConfig != null && p.equals(openConfig.path, configFile.path)) {
-            ref.read(openConfigProvider.notifier).close();
-          }
-          //delete the file next frame, to ensure the editor is closed
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            configFile.delete();
+            //if the editor is open on that file, close it
+            if (openConfig != null && p.equals(openConfig.path, configFile.path)) {
+              ref.read(openConfigProvider.notifier).close();
+            }
+            //delete the file next frame, to ensure the editor is closed
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              configFile.delete();
+              //TODO: Also delete the rendered map data (check if it exists first)
+              //($pwd/web/maps/map-id)
+            });
           });
         },
       ),
