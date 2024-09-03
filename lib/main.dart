5239017de3bfcc100babf961b1c6485f8f386dd2
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-09-03 02:59:11 +0200

    Add some explanation on (first) startup

--- lib/main.dart
+++ lib/main.dart
@@ -89,9 +89,24 @@ class MyHomePage extends ConsumerWidget {
             )
         ],
       ),
-      body: Center(
-        child: projectDirectory == null ? const PathPickerButton() : const DualPane(),
-      ),
+      body: projectDirectory == null
+          ? const Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("Select an empty folder to store your BlueMap files in:"),
+                  SizedBox(height: 8),
+                  PathPickerButton(),
+                  SizedBox(height: 8),
+                  Text("The BlueMap CLI tool will be downloaded into that folder."),
+                  SizedBox(height: 4),
+                  Text("It will generate some default config files for you."),
+                  SizedBox(height: 4),
+                  Text("You will then need to configure your maps in the BlueMap GUI."),
+                ],
+              ),
+            )
+          : const DualPane(),
     );
   }
 }
