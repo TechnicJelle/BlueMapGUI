8fe6faabe29812a180907f1789a045fe22471b46
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-09-06 23:19:11 +0200

    Better comments

--- lib/prefs.dart
+++ lib/prefs.dart
@@ -1,27 +1,27 @@
 import "package:shared_preferences/shared_preferences.dart";
 
 class Prefs {
-  // Constants
+  // == Constants ==
   static const String projectPathKey = "project_path";
 
-  // Static
+  // == Static ==
   static late Prefs _instance;
 
   static Prefs get instance => _instance;
 
-  // Private Variables
+  // == Private Variables ==
   final SharedPreferences _prefs;
 
-  // Constructors
+  // == Constructors ==
   Prefs._(this._prefs);
 
-  // Public Methods
+  // == Public Methods ==
   static Future<void> init() async {
     final prefs = await SharedPreferences.getInstance();
     _instance = Prefs._(prefs);
   }
 
-  // Getters and Setters
+  // == Getters and Setters ==
   String? get projectPath => _prefs.getString(projectPathKey);
 
   /// Set the project path. If `null`, the project path will be cleared.
