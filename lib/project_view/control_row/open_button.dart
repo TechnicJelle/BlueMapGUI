a87824b09bc38653f240adff66b86310a32d446c
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-09-22 04:07:19 +0200

    Animation to draw attention to the Open button when it becomes active
    Closes #61

--- lib/project_view/control_row/open_button.dart
+++ lib/project_view/control_row/open_button.dart
@@ -4,28 +4,67 @@ import "package:url_launcher/url_launcher.dart";
 
 import "control_row.dart";
 
-class OpenButton extends ConsumerWidget {
+class OpenButton extends ConsumerStatefulWidget {
   const OpenButton({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<OpenButton> createState() => _OpenButtonState();
+}
+
+class _OpenButtonState extends ConsumerState<OpenButton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+
+    _animation = CurveTween(curve: Curves.elasticOut).animate(_controller);
+
+    //set the animation as finished upon start, to prevent scale==0
+    _controller.value = 1.0;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final bool isRunning = ref.watch(
       processStateProvider.select(
         (asyncValue) => asyncValue.value == RunningProcessState.running,
       ),
     );
 
-    return ElevatedButton.icon(
-      onPressed: isRunning
-          ? () async {
-              final int port = ref.read(processProvider)?.port ?? 8100;
-              if (!await launchUrl(Uri.parse("http://localhost:$port"))) {
-                throw Exception("Could not launch url!");
+    ref.listen(processStateProvider, (previous, next) {
+      if (next.value == RunningProcessState.running) {
+        _controller.forward(from: 0);
+      }
+    });
+
+    return ScaleTransition(
+      scale: _animation,
+      child: ElevatedButton.icon(
+        onPressed: isRunning
+            ? () async {
+                final int port = ref.read(processProvider)?.port ?? 8100;
+                if (!await launchUrl(Uri.parse("http://localhost:$port"))) {
+                  throw Exception("Could not launch url!");
+                }
               }
-            }
-          : null,
-      label: const Text("Open"),
-      icon: const Icon(Icons.open_in_browser),
+            : null,
+        label: const Text("Open"),
+        icon: const Icon(Icons.open_in_browser),
+      ),
     );
   }
 }
