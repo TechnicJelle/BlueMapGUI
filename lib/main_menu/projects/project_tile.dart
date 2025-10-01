TechnicJelle/BlueMapGUI

commit bba82a3d8871f330009a1b0e73e0bf6c93d068ad

Fix error on fileWatcher create when projDir's parent doesn't exist

lib/main_menu/projects/project_tile.dart
@@ -85,11 +85,14 @@ class _PathPickerButtonState extends ConsumerState<ProjectTile> {
     super.initState();
     projectDirectoryExists = projectDirectory.existsSync();
 
-    fileWatchSub = projectDirectory.parent.watch().listen((FileSystemEvent event) {
-      projectDirectory.exists().then((bool exists) {
-        if (mounted) {
-          setState(() => projectDirectoryExists = exists);
-        }
+    final Directory parent = projectDirectory.parent;
+    parent.exists().then((bool parentExists) {
+      if (!parentExists) return;
+      fileWatchSub = parent.watch().listen((FileSystemEvent event) {
+        projectDirectory.exists().then((bool projectDirectoryExists) {
+          if (!mounted) return;
+          setState(() => projectDirectoryExists = projectDirectoryExists);
+        });
       });
     });
   }

commit 32a46b39697fbbb2f88d2c5818a60aa78c786a82

Fix deleted projects would not update in Projects List immediately
Regression from bba82a3d8871f330009a1b0e73e0bf6c93d068ad

lib/main_menu/projects/project_tile.dart
@@ -91,7 +91,7 @@ class _PathPickerButtonState extends ConsumerState<ProjectTile> {
       fileWatchSub = parent.watch().listen((FileSystemEvent event) {
         projectDirectory.exists().then((bool projectDirectoryExists) {
           if (!mounted) return;
-          setState(() => projectDirectoryExists = projectDirectoryExists);
+          setState(() => this.projectDirectoryExists = projectDirectoryExists);
         });
       });
     });