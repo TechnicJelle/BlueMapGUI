ebb0d943f4f1b6f976816f6996019c9f096dabae
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-10-22 22:30:32 +0200

    Update to BlueMap 5.13

--- lib/versions.dart
+++ lib/versions.dart
@@ -1,7 +1,7 @@
 // == Hardcoded BlueMap CLI JAR download URL and hash ==
-const blueMapTag = "5.12";
+const blueMapTag = "5.13";
 const blueMapCliJarHash =
-    "93eb5222580e8fba3b6873dd2735d25b5cf1c76a59ebb4c1dda27816fed4d293"; //SHA256
+    "3439ec94d27707a0a3c6beaa7a6f71457ef575d1cfd35e1c1b29c965d8850e91"; //SHA256
 
 // == Derived variables ==
 final blueMapCliJarUrl = Uri.https(
