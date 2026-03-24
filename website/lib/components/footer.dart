4a3987bdaec888c4b7c49bcade7fa4752ab977ec
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-03-24 03:07:06 +0100

    Add non-affiliation disclaimer to footer

--- website/lib/components/footer.dart
+++ website/lib/components/footer.dart
@@ -5,33 +5,44 @@ import "../constants.dart";
 Footer generateFooter() {
   return Footer(
     children: [
-      Span(
+      Div(
+        classes: ["footer-top"],
         children: [
-          A.newTab(
-            href: repo,
-            children: [T("⭐ Star on GitHub")],
+          Span(
+            classes: ["footer-links"],
+            children: [
+              A.newTab(
+                href: repo,
+                children: [T("⭐ Star on GitHub")],
+              ),
+              A.newTab(
+                href: "https://github.com/sponsors/TechnicJelle",
+                children: [T("Sponsor")],
+              ),
+              A.newTab(
+                href: "https://ko-fi.com/technicjelle",
+                children: [T("Support on Ko-fi")],
+              ),
+            ],
           ),
-          A.newTab(
-            href: "https://github.com/sponsors/TechnicJelle",
-            children: [T("Sponsor")],
-          ),
-          A.newTab(
-            href: "https://ko-fi.com/technicjelle",
-            children: [T("Support on Ko-fi")],
+          Span(
+            classes: ["updated-on"],
+            children: [
+              T("Website last updated on "),
+              Time(
+                datetime: DateTime.now().toIso8601String(),
+                visible: DateTime.now()
+                    .copyWith(microsecond: 0)
+                    .toIso8601String()
+                    .replaceAll("T", " "),
+              ),
+            ],
           ),
         ],
       ),
-      Span(
-        children: [
-          T("Website last updated on "),
-          Time(
-            datetime: DateTime.now().toIso8601String(),
-            visible: DateTime.now()
-                .copyWith(microsecond: 0)
-                .toIso8601String()
-                .replaceAll("T", " "),
-          ),
-        ],
+      P.text(
+        "This website and BlueMap GUI are neither officially affiliated with Minecraft, Mojang, Microsoft or BlueMap.",
+        classes: ["disclaimer"],
       ),
     ],
   );
