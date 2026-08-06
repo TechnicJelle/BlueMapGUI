23b1f20470410e675bb69efecf68422be5e20e9d
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-08-06 21:52:35 +0200

    Update to BM 5.23

--- lib/versions.dart
+++ lib/versions.dart
@@ -1,7 +1,7 @@
 // == Hardcoded BlueMap CLI JAR download URL and hash ==
-const blueMapTag = "5.22";
+const blueMapTag = "5.23";
 const blueMapCliJarHash =
-    "a2289d649f6a83e04cf222abcd3d4f2df4e32e61147015f921c05066f63eefb7"; //SHA256
+    "ebdb33821b127505be94599555cb16bb9b46c8f70aa22283d314cdb829b47a54"; //SHA256
 
 // == Derived variables ==
 final blueMapCliJarUrl = Uri.https(
@@ -14,7 +14,7 @@ const String version = String.fromEnvironment("version", defaultValue: _vDev);
 
 // == Java Managed ==
 // From https://adoptium.net/temurin/releases?version=25&os=any&arch=any
-const String javaManagedVersion = "jdk-25.0.3+9";
+const String javaManagedVersion = "jdk-25.0.4+7";
 const String javaManagedLinuxX64Hash =
     "487ad434d8b121ae3902d5ad9cb830cd8e1f75fefad6e2ba80f89d60e3db95d7"; //SHA256
 const String javaManagedWindowsX64Hash =
