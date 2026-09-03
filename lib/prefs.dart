TechnicJelle/BlueMapGUI

commit 60477ec83d4fbdf2a64d4f578ce552048033fce6

Improved handling of Java process Results

lib/prefs.dart
@@ -1,9 +1,12 @@
 import "dart:async";
 import "dart:io";
+import "dart:typed_data";
 
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:shared_preferences/shared_preferences.dart";
+
+import "utils.dart";
 
 late final SharedPreferencesWithCache _prefs;
 
@@ -24,24 +27,51 @@ Future<void> initPrefs() async {
 
 enum JavaPathMode { unset, system, managed, custom }
 
+final class JavaResult {
+  /// Exit code for the process.
+  ///
+  /// See [Process.exitCode] for more information in the exit code value.
+  final int exitCode;
+
+  /// Standard output from the process.
+  final String stdout;
+
+  /// Standard error from the process.
+  final String stderr;
+
+  /// Process id of the process.
+  final int pid;
+
+  JavaResult(this.pid, this.exitCode, Uint8List stdout, Uint8List stderr)
+    : stdout = processOutputToString(stdout),
+      stderr = processOutputToString(stderr);
+
+  JavaResult.from(ProcessResult processResult)
+    : pid = processResult.pid,
+      exitCode = processResult.exitCode,
+      stdout = processOutputToString(processResult.stdout),
+      stderr = processOutputToString(processResult.stderr);
+}
+
 class JavaPath {
   JavaPathMode type;
   String path;
 
   JavaPath(this.type, this.path);
 
-  Future<ProcessResult> run({
+  Future<JavaResult> run({
     required List<String> args,
     Directory? workingDirectory,
   }) async {
-    return Process.run(
+    final ProcessResult processResult = await Process.run(
       path,
       args,
       workingDirectory: workingDirectory?.path,
     );
+    return JavaResult.from(processResult);
   }
 
-  Future<ProcessResult> runJar(
+  Future<JavaResult> runJar(
     File jar, {
     List<String> jvmArgs = const [],
     List<String> processArgs = const [],
@@ -66,24 +96,24 @@ class JavaPath {
     );
   }
 
-  Future<ProcessResult> runJarTimeout(
+  Future<JavaResult> runJarTimeout(
     File jar,
     Duration timeout, {
     List<String> jvmArgs = const [],
     List<String> processArgs = const [],
     Directory? workingDirectory,
   }) async {
-    final StringBuffer stdoutBuffer = StringBuffer();
-    final StringBuffer stderrBuffer = StringBuffer();
-    final Process process =
-        await startJar(
-            jar,
-            jvmArgs: jvmArgs,
-            processArgs: processArgs,
-            workingDirectory: workingDirectory,
-          )
-          ..stdout.listen((List<int> e) => e.forEach(stdoutBuffer.writeCharCode))
-          ..stderr.listen((List<int> e) => e.forEach(stderrBuffer.writeCharCode));
+    // These are actually Uint8's (raw bytes 0..255 from the OS pipe)
+    final List<int> stdoutBuffer = [];
+    final List<int> stderrBuffer = [];
+    final Process process = await startJar(
+      jar,
+      jvmArgs: jvmArgs,
+      processArgs: processArgs,
+      workingDirectory: workingDirectory,
+    );
+    final stdoutSub = process.stdout.listen(stdoutBuffer.addAll);
+    final stderrSub = process.stderr.listen(stderrBuffer.addAll);
 
     //We kill the process after the duration
     bool wasKilled = false;
@@ -91,8 +121,13 @@ class JavaPath {
       if (process.kill()) wasKilled = true;
     });
 
-    //If the process has already stopped, we cancel the killer
+    // Wait for process to exit
     final int exitCode = await process.exitCode;
+    // Wait for stdStreams to drain
+    await Future.wait([stdoutSub.asFuture<void>(), stderrSub.asFuture<void>()]);
+    // Cancel the stream subscriptions
+    await Future.wait([stdoutSub.cancel(), stderrSub.cancel()]);
+    //If the process has already stopped, we cancel the killer
     killer.cancel();
 
     if (wasKilled) {
@@ -103,11 +138,11 @@ class JavaPath {
       );
     }
 
-    return ProcessResult(
+    return JavaResult(
       process.pid,
       exitCode,
-      stdoutBuffer.toString(),
-      stderrBuffer.toString(),
+      Uint8List.fromList(stdoutBuffer),
+      Uint8List.fromList(stderrBuffer),
     );
   }
 }

lib/main_menu/projects/project_tile.dart
@@ -308,7 +308,7 @@ It will only be removed from the list.""",
       tempMapsDir = null;
     }
 
-    final ProcessResult run;
+    final JavaResult run;
     try {
       run = await javaPath.runJarTimeout(
         bluemapJar,
@@ -321,8 +321,8 @@ It will only be removed from the list.""",
           .error(error: .runFail, details: e.toString());
       return;
     }
-    final String stdout = run.stdout.toString();
-    final String stderr = run.stderr.toString();
+    final String stdout = run.stdout;
+    final String stderr = run.stderr;
 
     final bool startSuccess = stdout.contains("Generated default config files for you");
 

lib/main_menu/settings/java/check_java_version.dart
@@ -30,9 +30,9 @@ Future<int> checkJavaVersion(JavaPath javaPath) async {
   }
 
   try {
-    final ProcessResult jv = await javaPath.run(args: ["-fullversion"]);
+    final JavaResult jv = await javaPath.run(args: ["-fullversion"]);
     final int exitCode = jv.exitCode;
-    final String stderr = jv.stderr.toString();
+    final String stderr = jv.stderr;
 
     if (exitCode != 0) {
       throw JavaVersionCheckException("Process exited with $exitCode.\n$stderr");

lib/project_view/configs/models/base.dart
@@ -136,14 +136,14 @@ class ConfigFile<T extends BaseConfigModel> {
     }
 
     final List<String> args = files.map((f) => f.path).toList();
-    final ProcessResult result = await javaPath.runJar(_hoconFile!, processArgs: args);
+    final JavaResult result = await javaPath.runJar(_hoconFile!, processArgs: args);
 
     final int exitCode = result.exitCode;
-    final String stderr = result.stderr.toString();
+    final String stderr = result.stderr;
     if (exitCode != 0) {
       throw FatalConfigFileRunException(exitCode: exitCode, stderr: stderr);
     }
-    final String stdout = result.stdout.toString();
+    final String stdout = result.stdout;
     final List<String> jsons = stdout.split(String.fromCharCode(0));
 
     return List.generate(files.length, (int index) {

lib/utils.dart
@@ -1,5 +1,7 @@
 import "dart:async";
+import "dart:convert";
 import "dart:io";
+import "dart:typed_data";
 
 import "package:crypto/crypto.dart";
 import "package:flutter/material.dart";
@@ -68,6 +70,14 @@ Color colorFromHexColour(String hexString) {
   }
 
   throw Exception("Failed to parse colour code: $hexString");
+}
+
+String processOutputToString(dynamic stdStream) {
+  return switch (stdStream) {
+    final String s => s,
+    final Uint8List l => utf8.decode(l),
+    _ => throw UnsupportedError("Unknown Type: `${stdStream.runtimeType}`! Cannot process this type to a String!"),
+  };
 }
 
 /// Checks if the given file has the same SHA256 hash as the given hash.

commit d84b517529545fa805a0d97e427cde1c8b03933d

Fix project not opening (stuck on `_OpeningStep.running`)
Regression introduced in 60477ec83d4fbdf2a64d4f578ce552048033fce6

lib/prefs.dart
@@ -106,14 +106,25 @@ class JavaPath {
     // These are actually Uint8's (raw bytes 0..255 from the OS pipe)
     final List<int> stdoutBuffer = [];
     final List<int> stderrBuffer = [];
+
+    // Track stream completion to ensure all output is captured (see Process.exitCode's docs' last paragraph)
+    final stdoutDone = Completer<void>();
+    final stderrDone = Completer<void>();
+
     final Process process = await startJar(
       jar,
       jvmArgs: jvmArgs,
       processArgs: processArgs,
       workingDirectory: workingDirectory,
     );
-    final stdoutSub = process.stdout.listen(stdoutBuffer.addAll);
-    final stderrSub = process.stderr.listen(stderrBuffer.addAll);
+    final stdoutSub = process.stdout.listen(
+      stdoutBuffer.addAll,
+      onDone: stdoutDone.complete,
+    );
+    final stderrSub = process.stderr.listen(
+      stderrBuffer.addAll,
+      onDone: stderrDone.complete,
+    );
 
     //We kill the process after the duration
     bool wasKilled = false;
@@ -124,7 +135,7 @@ class JavaPath {
     // Wait for process to exit
     final int exitCode = await process.exitCode;
     // Wait for stdStreams to drain
-    await Future.wait([stdoutSub.asFuture<void>(), stderrSub.asFuture<void>()]);
+    await Future.wait([stdoutDone.future, stderrDone.future]);
     // Cancel the stream subscriptions
     await Future.wait([stdoutSub.cancel(), stderrSub.cancel()]);
     //If the process has already stopped, we cancel the killer