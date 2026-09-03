TechnicJelle/BlueMapGUI

commit d59c99d8fecac99f5629558c35be0ae60cccba81

Switch to the new `material_ui` package

lib/project_view/configs/advanced_editor.dart
@@ -1,8 +1,8 @@
 import "dart:async";
 import "dart:io";
 
-import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:material_ui/material_ui.dart";
 import "package:path/path.dart" as p;
 import "package:re_editor/re_editor.dart";
 import "package:re_highlight/languages/yaml.dart" show langYaml;

lib/confirmation_dialog.dart
@@ -1,6 +1,6 @@
 import "dart:async";
 
-import "package:flutter/material.dart";
+import "package:material_ui/material_ui.dart";
 
 void showConfirmationDialog({
   required BuildContext context,

lib/delete_icon.dart
@@ -1,4 +1,4 @@
-import "package:flutter/material.dart";
+import "package:material_ui/material_ui.dart";
 
 class DeleteIcon extends StatefulWidget {
   const DeleteIcon({super.key});

lib/hover.dart
@@ -1,4 +1,4 @@
-import "package:flutter/material.dart";
+import "package:material_ui/material_ui.dart";
 
 class Hover extends StatefulWidget {
   final Widget alwaysChild;

lib/main.dart
@@ -1,8 +1,8 @@
 import "dart:async";
 import "dart:io";
 
-import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:material_ui/material_ui.dart";
 import "package:path/path.dart" as p;
 import "package:url_launcher/url_launcher_string.dart";
 import "package:window_manager/window_manager.dart";

lib/main_menu/main_menu.dart
@@ -1,5 +1,5 @@
-import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:material_ui/material_ui.dart";
 
 import "../sidebar.dart";
 import "projects/projects_screen.dart";

lib/main_menu/projects/new_project_dialog.dart
@@ -2,8 +2,8 @@ import "dart:async";
 import "dart:io";
 
 import "package:file_picker/file_picker.dart";
-import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:material_ui/material_ui.dart";
 import "package:path/path.dart" as p;
 import "package:path_provider/path_provider.dart";
 

lib/main_menu/projects/project_tile.dart
@@ -2,9 +2,9 @@ import "dart:async";
 import "dart:convert";
 import "dart:io";
 
-import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:material_ui/material_ui.dart";
 import "package:path/path.dart" as p;
 import "package:url_launcher/url_launcher.dart";
 

commit d85b42d5a7bab18f88ea2bc186d4dca3b987e587

Fix AdvancedEditor text being too dark in dark mode
Somehow, this was caused by d59c99d8fecac99f5629558c35be0ae60cccba81

lib/project_view/configs/advanced_editor.dart
@@ -150,6 +150,10 @@ class _AdvancedEditorState extends ConsumerState<AdvancedEditor> {
           );
         },
         style: CodeEditorStyle(
+          textColor: switch (brightness) {
+            Brightness.dark => Colors.white,
+            Brightness.light => Colors.black,
+          },
           fontFamily: pixelCode200.fontFamily,
           fontSize: pixelCode200.fontSize,
           fontHeight: pixelCode200.height,